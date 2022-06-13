7794c632bcfe2f9e1e629d298897bf9bc8e53e49
muccidavide <davidemucci0804@gmail.com>
2022-06-13 15:32:01 +0200

    Add Sass Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,15 +19,15 @@
         <div class="row justify-content-center"> 
         @forelse($movies as $movie)
 
-            <div class="col-4">
+            <div class="col-4 g-3">
             
                 <div class="card_movie m-auto">
                     <img class="img-fluid w-100" src='https://picsum.photos/300/200' alt='default img'>
-                    <div class="card_details">
+                    <div class="card_details p-2 mt-2">
                         <h3>{{ $movie['title'] }}</h3>
-                        <p>{{ $movie['nationality'] }}</p>
-                        <p>{{ $movie['date'] }}</p>
-                        <p>{{ $movie['vote'] }}</p>
+                        <p><strong>Nation: </strong>{{ $movie['nationality'] }}</p>
+                        <p><strong>Date: </strong>{{ $movie['date'] }}</p>
+                        <p><strong>Vote: </strong>{{ $movie['vote'] }}</p>
                     </div>
 
                 </div>
